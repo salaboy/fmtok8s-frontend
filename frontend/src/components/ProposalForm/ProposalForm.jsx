9e7a1112b1b064955c1b154e9f54e29e0b21e760
salaboy <Salaboy@gmail.com>
2022-06-16 17:17:16 +0100

    generated proposals text

--- frontend/src/components/ProposalForm/ProposalForm.jsx
+++ frontend/src/components/ProposalForm/ProposalForm.jsx
@@ -5,6 +5,7 @@ import cn from 'classnames';
 import Button from '../../components/Button/Button'
 import TextField from '../../components/Form/TextField/TextField'
 import TextArea from '../../components/Form/TextArea/TextArea'
+import { LoremIpsum } from "lorem-ipsum";
 
 function ProposalForm() {
 
@@ -12,10 +13,30 @@ function ProposalForm() {
   const [author, setAuthor] = useState('');
   const [email, setEmail] = useState('');
   const [description, setDescription] = useState('');
+  const [generated, setGenerated] = useState(false);
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [sended, setSended] = useState(false);
   const [data, setData] = useState(null);
+
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4
+    }
+  });
+
+  function generate(){
+    setDescription(lorem.generateParagraphs(2));
+    setTitle(lorem.generateSentences(5));
+    setAuthor(lorem.generateWords(2));
+    setEmail(lorem.generateWords(1)+"@mail.com");
+    setGenerated(true);
+  }
 
   const handleSubmit = () => {
     setLoading(true);
@@ -54,19 +75,29 @@ function ProposalForm() {
           <h3>Submit your proposal</h3>
           <div className="Form">
             <div className="FormField">
-              <TextField label="Title" name="title" value={title}  changeHandler={e => setTitle(e.target.value)} />
+              <TextField label="Title" name="title" value={title} inputProps={
+                { readOnly: true, }
+              }   />
             </div>
             <div className="FormField">
-              <TextField label="Author" name="author" value={author} changeHandler={e => setAuthor(e.target.value)} />
+              <TextField label="Author" name="author" value={author} inputProps={
+                { readOnly: true, }
+              }  />
             </div>
             <div className="FormField">
-              <TextField label="Email" name="email" value={email} changeHandler={e => setEmail(e.target.value)} />
+              <TextField label="Email" name="email" value={email} inputProps={
+                { readOnly: true, }
+              }  />
             </div>
             <div className="FormField">
-              <TextArea label="Abstract" name="description" value={description} changeHandler={e => setDescription(e.target.value)} />
+              <TextArea label="Abstract" name="description" inputProps={
+                { readOnly: true, }
+              } value={description} />
             </div>
             {isError && <small className="mt-3 d-inline-block text-danger">Something went wrong. Please try again later.</small>}
-            <Button main clickHandler={handleSubmit} disabled={loading}>{loading ? 'Loading...' : 'Submit'}</Button>
+            <Button main clickHandler={generate} disabled={generated}>Generate</Button>
+            <Button main clickHandler={handleSubmit} disabled={loading || !generated}>{loading ? 'Loading...' : 'Submit'}</Button>
+
 
 
 
