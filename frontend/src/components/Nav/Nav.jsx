c3a96c6379831ece01b86902042eaa322e587393
salaboy <Salaboy@gmail.com>
2022-06-22 09:52:53 +0100

    fixing speakers variable

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,63 +1,64 @@
 import "./Nav.scss";
-import React, { useEffect, useState, useContext, useRef } from "react";
-import { NavLink} from 'react-router-dom'
+import React, {useEffect, useState, useContext, useRef} from "react";
+import {NavLink} from 'react-router-dom'
 import AppContext from '../../contexts/AppContext';
 import cn from 'classnames';
 
 function Nav() {
-  const {  currentSection } = useContext(AppContext);
+    const {currentSection} = useContext(AppContext);
 
     let speakersEnabled = window._env_.FEATURE_SPEAKERS_ENABLED
     let ticketsEnabled = window._env_.FEATURE_TICKETS_ENABLED
     let c4pEnabled = window._env_.FEATURE_C4P_ENABLED
 
     return (
-      <div className={  cn({
-          ["Nav"]: true,
-          ["--backoffice"]: currentSection === "back-office",
+        <div className={cn({
+            ["Nav"]: true,
+            ["--backoffice"]: currentSection === "back-office",
         })}>
-        <div className="Nav__container">
-          {currentSection !== "back-office" && (
-            <div className="Nav__main">
+            <div className="Nav__container">
+                {currentSection !== "back-office" && (
+                    <div className="Nav__main">
 
               <span>
                 <NavLink activeClassName='--active' to='/agenda-page' exact> Agenda</NavLink>
               </span>
-              <span>
-                   { speakersEnabled && (
-                <NavLink activeClassName='--active' to='/speakers-page' exact> Speakers</NavLink>
-                   )}
-              </span>
-                { ticketsEnabled && (
-                  <span>
+                        {speakersEnabled && (
+                            <span>
+                             <NavLink activeClassName='--active' to='/speakers-page' exact> Speakers</NavLink>
+                            </span>
+                        )}
+                        {ticketsEnabled && (
+                            <span>
                     <NavLink activeClassName='--active' to='/tickets-page' exact> Tickets</NavLink>
                   </span>
-                )}
-                { c4pEnabled && (
-              <span>
+                        )}
+                        {c4pEnabled && (
+                            <span>
                 <NavLink activeClassName='--active' to='/proposals-page' exact> Proposals</NavLink>
               </span>
-                )}
-              <span>
+                        )}
+                        <span>
                 <NavLink activeClassName='--active' to='/about-page' exact> About</NavLink>
               </span>
-            </div>
-          )}
-          <div className="Nav__back-office">
-            {currentSection !== "back-office" && (
-              <span>
+                    </div>
+                )}
+                <div className="Nav__back-office">
+                    {currentSection !== "back-office" && (
+                        <span>
                 <NavLink activeClassName='--active' to='/back-office-page' exact> -> </NavLink>
               </span>
-            )}
-            {currentSection === "back-office" && (
-              <span>
+                    )}
+                    {currentSection === "back-office" && (
+                        <span>
                 <NavLink activeClassName='--active' to='/' exact> Back to Site </NavLink>
               </span>
-            )}
-          </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
     );
 
 }
+
 export default Nav;
--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ echo "window._env_ = {" >> ./static/env-config.js
 # Append configuration property to JS file
 echo "  FEATURE_TICKETS_ENABLED: \"$FEATURE_TICKETS_ENABLED\"," >> ./static/env-config.js
 echo "  FEATURE_C4P_ENABLED: \"$FEATURE_C4P_ENABLED\"," >> ./static/env-config.js
-echo "  FEATURE_SPEAKERS_ENABLED: \"$FEATURE_C4P_ENABLED\"," >> ./static/env-config.js
+echo "  FEATURE_SPEAKERS_ENABLED: \"$FEATURE_SPEAKERS_ENABLED\"," >> ./static/env-config.js
 
 echo "}" >> ./static/env-config.js
 
