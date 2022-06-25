2f92891dd4253b4b4599418054cff5ad74b6d248
salaboy <Salaboy@gmail.com>
2022-06-25 10:40:14 +0100

    env false if they are not set

--- startup.sh
+++ startup.sh
@@ -8,11 +8,11 @@ touch ./static/env-config.js
 echo "window._env_ = {" >> ./static/env-config.js
 
 # Append configuration property to JS file
-echo "  FEATURE_TICKETS_ENABLED: \"$FEATURE_TICKETS_ENABLED\"," >> ./static/env-config.js
-echo "  FEATURE_C4P_ENABLED: \"$FEATURE_C4P_ENABLED\"," >> ./static/env-config.js
-echo "  FEATURE_SPEAKERS_ENABLED: \"$FEATURE_SPEAKERS_ENABLED\"," >> ./static/env-config.js
-echo "  FEATURE_DEBUG_ENABLED: \"$FEATURE_DEBUG_ENABLED\"," >> ./static/env-config.js
-echo "  FEATURE_ALTERNATIVE_HERO_ENABLED: \"$FEATURE_ALTERNATIVE_HERO_ENABLED\"," >> ./static/env-config.js
+[[ ! -z $FEATURE_TICKETS_ENABLED ]] && echo "  FEATURE_TICKETS_ENABLED: \"$FEATURE_TICKETS_ENABLED\"" || echo  "  FEATURE_TICKETS_ENABLED: \"false\"," >> ./static/env-config.js
+[[ ! -z $FEATURE_C4P_ENABLED ]] && echo "  FEATURE_C4P_ENABLED: \"$FEATURE_C4P_ENABLED\"" || echo  "  FEATURE_C4P_ENABLED: \"false\"," >> ./static/env-config.js
+[[ ! -z $FEATURE_SPEAKERS_ENABLED ]] && echo "  FEATURE_SPEAKERS_ENABLED: \"$FEATURE_SPEAKERS_ENABLED\"" || echo  "  FEATURE_SPEAKERS_ENABLED: \"false\"," >> ./static/env-config.js
+[[ ! -z $FEATURE_DEBUG_ENABLED ]] && echo "  FEATURE_DEBUG_ENABLED: \"$FEATURE_DEBUG_ENABLED\"" || echo  "  FEATURE_DEBUG_ENABLED: \"false\"," >> ./static/env-config.js
+[[ ! -z $FEATURE_ALTERNATIVE_HERO_ENABLED ]] && echo "  FEATURE_ALTERNATIVE_HERO_ENABLED: \"$FEATURE_ALTERNATIVE_HERO_ENABLED\"" || echo  "  FEATURE_ALTERNATIVE_HERO_ENABLED: \"false\"," >> ./static/env-config.js
 
 
 echo "}" >> ./static/env-config.js
